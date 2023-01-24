7bbb02b5a4127829cc22688fdae83157b88595be
halisa <halisasaipulaa@gmail.com>
2023-01-24 10:16:26 -0800

    rendered Exercise Videos Component wiht Youtube video and Similar Exercises Component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -9,6 +9,8 @@ import SimilarExercises from '../components/SimilarExercises';
 function ExerciseDetail() {
     const [exerciseDetail, setExerciseDetail] = useState({});
     const [exerciseVideos, setExerciseVideos] = useState([]);
+    const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+    const [equipmentExercises, setEquipmentExercises] = useState([]);
     const { id } = useParams();
 
     useEffect(() => {
@@ -20,8 +22,14 @@ function ExerciseDetail() {
             console.log(exerciseDetailData)
             setExerciseDetail(exerciseDetailData);
 
-            const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
-            setExerciseVideos(exerciseVideosData)
+            const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
+            setExerciseVideos(exerciseVideosData.contents);
+
+            const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+            setTargetMuscleExercises(targetMuscleExercisesData);
+
+            const equimentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+            setEquipmentExercises(equimentExercisesData);
         
         }
         fetchExercisesData()
@@ -31,7 +39,7 @@ function ExerciseDetail() {
         <Box>
             <Detail exerciseDetail={exerciseDetail}/>
             <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
-            <SimilarExercises />
+            <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}/>
         </Box>
     )
 }
