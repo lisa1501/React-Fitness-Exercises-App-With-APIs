dfde7331a28aa95569f26c16d65f7ff653cb2bae
Lisa S <halisasaipulla@gmail.com>
2023-02-15 14:27:16 -0800

    upload images

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,20 +6,20 @@ import HeroBannerImage from '../assets/images/banner.png'
 const HeroBanner = () => {
     return (
         <Box sx={{
-            mt:{ lg:'40px', xs:'10px'},
-            ml:{ sm:'200px'}}} 
+            mt:{ lg:'212px', xs:'70px'},
+            ml:{ sm:'50px'}}} 
             position="relative" p="20px" >
             <Typography color="#FF2625" fontWeight="600"
-                fontSize="26px">
+                fontSize="36px">
                 Fitness Club
             </Typography>
             <Typography fontWeight={700}
-            xs={{ fontSize:{lg:'44px', xs:'40px'}}}
+            xs={{ fontSize:{lg:'54px', xs:'50px'}}}
             mb="23px" mt="30px">
                 Sweat, Smile <br /> 
                 And Repeat
             </Typography>
-            <Typography fontSize="22px" lineHeight="35px" mb={4}>
+            <Typography fontSize="32px" lineHeight="35px" mb={4}>
                 Check out the most effective exercises
             </Typography>
             <Button variant='contained' color="error" href="#exercises"
